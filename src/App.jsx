600e0b7bf7ec6a1c11804ee4929a7f38636983bd
ragOP <najmiraghib@gmail.com>
2026-02-05 11:59:25 +0530

    ds

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,13 @@ const App = () => {
     <Router>
       <Routes>
       <Route path="/" element={<Home />} />
-        <Route path="/engsf1" element={<Chatbotdq />} />
+        {/* <Route path="/engsf1" element={<Chatbotdq />} /> */}
         <Route path="/engsfdq" element={<Chatbotdq2 />} />
-        <Route path="/engsf2200" element={<ChatbotTwo />} />
+        {/* <Route path="/engsf2200" element={<ChatbotTwo />} />
         <Route path="/engsf1dup" element={<Chatbotdq2 />} />
         <Route path="/engsffast" element={<FastChat />} />
         <Route path="/engsf2200dup" element={<ChatbotFour />} />
-          <Route path="/engimg" element={<Raghib />} />
+          <Route path="/engimg" element={<Raghib />} /> */}
       </Routes>
     </Router>
   );
